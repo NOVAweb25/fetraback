e14adb7f5989681585c8afe984a95bff3ab852fe
NOVAweb25 <novaweb@outlook.sa>
2025-12-07 02:39:33 +0300

    DFR

--- app.js
+++ app.js
@@ -25,7 +25,6 @@ const notificationRoutes = require("./src/routes/notificationRoutes");
 const settingRoutes = require("./src/routes/settingRoutes");
 const statsRoutes = require("./src/routes/statsRoutes");
 const reviewRoutes = require("./src/routes/reviewRoutes");
-const paymentRoutes = require("./src/routes/payment");
 
 
 dotenv.config();
@@ -79,7 +78,7 @@ app.use("/api/notifications", notificationRoutes);
 app.use("/api/settings", settingRoutes);
 app.use("/api/stats", statsRoutes);
 app.use("/api/reviews", reviewRoutes);
-app.use("/api/payment", paymentRoutes);
+
 
 
 // 🖼️ مسار ثابت للصور
